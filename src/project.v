0514848e7aaedb229a9c2ef9908b74ae2735d38d
Tomvdsch <82215862+Tomvdsch@users.noreply.github.com>
2026-09-07 10:46:17 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -21,7 +21,7 @@ module tt_um_tomvdsch_tiny32_soc (
         .ENABLE_A(0),
         .ENABLE_U(0),
         .ENABLE_COUNTERS(0),
-        .ICACHE_WORDS(2),
+        .ICACHE_WORDS(1),
         .SYS_CLK_HZ(25000000)
     ) u_soc (
         .clk(clk),

8833f196b9bacdab8262234b458249058b3b58a1
Tomvdsch <82215862+Tomvdsch@users.noreply.github.com>
2026-09-07 22:08:23 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -21,10 +21,10 @@ module tt_um_tomvdsch_tiny32_soc (
         .ENABLE_A(0),
         .ENABLE_U(0),
         .ENABLE_COUNTERS(0),
-        .ENABLE_SPI(1),
+        .ENABLE_SPI(0),
         .ENABLE_I2C(1),
         .ENABLE_TIMER1(1),
-        .ENABLE_WATCHDOG(1),
+        .ENABLE_WATCHDOG(0),
         .PWM_CHANNELS(5),
         .ICACHE_WORDS(1),
         .SYS_CLK_HZ(25000000)
